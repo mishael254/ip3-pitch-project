1437ce99cdae35dcbd9e3541117e7cab556dd4aa
mishael254 <mishaelmishantoz@gmail.com>
2020-07-22 15:13:23 +0300

    import the following modules; flask,bootstrap, login manager from flask login, sqlalchemy and config options from config file also  register the blue print

---
+++ app/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+
+
+    app = Flask(__name__)
+
+
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    
+    login_manager.init_app(app)
+
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+    return app
