f8d3a51a1ee84336695cffb07ff07a316d33bcf0
mishael254 <mishaelmishantoz@gmail.com>
2020-07-23 15:19:25 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -9,6 +9,7 @@ from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
+app = create_app('production')
 
 migrate = Migrate(app,db)
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=ndegwamishael@gmail.com
+export MAIL_PASSWORD=mishael2019
+python3.6 manage.py server
